42e8b91604b283280d2f7adc5f49ab980ff60c9c
CHJH <c.hartgerink@gmail.com>
2015-11-14 23:03:07 +0100

    Add test download from PLOS

--- functions/download_results.R
+++ functions/download_results.R
@@ -11,6 +11,6 @@ resultstext <- searchplos('*:*',
            c('id', 'publication_date', 'results_and_discussion'),
            fq = 'doc_type:full',
            start = 0,
-           limit = 3) # need to replace this with hits
+           limit = 10) # need to replace this with hits
 
 save(resultstext, file = sprintf('data/%s_resultstext', format(Sys.time(), "%Y%m%d")))
